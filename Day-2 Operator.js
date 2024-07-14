f26b14c0ca76baf88842eef5380227299e44ba0c
poison291 <shahsusan291@gmail.com>
2024-07-14 08:53:01 +0545

    Day 2 done

---
+++ Day-2 Operator.js
@@ -0,0 +1,80 @@
+//! Activities 1: Arithmetic Operators
+
+//? Task 1
+let a = 5;
+let b = 6;
+console.log(a + b); //* output 11
+
+//? Task 2
+let c = 10;
+let d = 4;
+console.log(c - d); //* output 6
+
+//? Task  3
+let e = 5;
+let f = 2;
+console.log(e * f); //* output 10
+
+//? Task  4
+let g = 10;
+let h = 2;
+console.log(g / h); //* output 5
+
+//? Task 5
+let i = 5;
+let j = 2;
+console.log(i % j); //* output 1
+
+//! Activity 2: Assignment Operators
+
+//? Task 6
+let k = 5;
+k += 10;
+console.log(k);//* Output 15
+
+//? Task 7
+let l = 5;
+l -= 2;
+console.log(l);//* Output 3
+
+//! Activity 3: comparision Operators
+
+//? Task 8
+let m = 5;
+let n = 10;
+console.log(m < n);//* Output true
+console.log(m > n);//* Output false
+
+//? Task 9
+let o = 15;
+let p = 20;
+console.log(0 <= p);//* output true
+console.log(0 >= p);//* output false
+
+//? Task 10
+let q = 15;
+let r = "15";
+console.log(q == r);//* true(loose equality)
+console.log(q === r);//* false(strict equality)
+
+//*In strict equality(===) the type of data and the value of data be equal but in Loose equaltiy(==) The only value of data needs to be equal...
+
+//! Activity 4: Logical operators
+
+//? Task 11, 12, 13
+console.log(true && true);//* true
+console.log(false || true);//* true
+console.log(true != true);//* false
+
+//! Activity 5: Ternary Operator
+
+//? Task 14
+ let age = 25;
+ let license = (age > 18) ? "Yes" : "No";
+ console.log(license);//*Yes
+  //? If age is greater than 18 then the value of license will be Yes otherwise No
+
+  //? Day 2 completed
+
+
+
