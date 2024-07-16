b102eb6970878bda2d6f18651ca25266239d7c4e
poison291 <shahsusan291@gmail.com>
2024-07-16 19:25:55 +0545

    DAy 4

---
+++ Day-4 loop.js
@@ -0,0 +1,58 @@
+//! Task 1
+// for (let i = 0; i <=10; i++) {
+//     console.log( i);
+// }
+
+//! Task 2
+// for(let i = 1;i<=10; i++){
+//     console.log("5 * 1 =" +  5 * i);
+// }
+//! TAsk 3
+// function sum1() {
+    
+//     let sum = 0;
+//     let i = 1;
+//     while (i<=10) {
+//         i++;
+//         if(i % 2 == 0){
+        
+//             sum = sum + i;
+//         }
+//     }
+//     console.log(sum);
+// }
+//     // console.log(sum);
+//     sum1()
+
+    //! task 4
+    // let i = 10;
+    // while (i > 0) {
+    //     console.log(i);
+    //     i--
+    // }
+
+    //! task 5
+    
+// let i = 1;
+//     do {
+//         console.log(i);
+//         i++
+//     } while (i<=5);
+
+
+//! Task 8
+// for(let i = 1;i<=10;i++){
+// if (i == 5) {
+//    continue;
+// }
+// console.log(i);
+// }
+
+//! Task 9
+for(let i = 1;i<=10;i++){
+    if (i == 7) {
+       break;
+    }
+    console.log(i);
+    }
+
